e341c6532d73bc99b81407ee077e704aa842a413
limcuili <limcuili@hotmail.com>
2018-11-30 12:54:14 +0000

    framework for methods needed

---
+++ HuffmanList.java
@@ -0,0 +1,17 @@
+public class HuffmanList {
+
+    // initialize list data structure taking values of type HuffmanNode.
+
+    public void insert(HuffmanNode n) {
+        // inserts new item of value n into the list
+    }
+
+    public HuffmanNode extractMin() {
+        // returns the item of the list whose HuffmanNode has minimum value and also deletes this item from the list
+    }
+
+    public boolean containsAtMostOneItem() {
+        // returns true if the list contains zero or one item, and false otherwise.
+    }
+
+}
