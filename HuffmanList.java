e7ada11d9f9c4cae9e2b4165ccb717153c2a4523
limcuili <limcuili@hotmail.com>
2018-12-11 11:47:47 +0000

    full commit of HuffmanList

--- HuffmanList.java
+++ HuffmanList.java
@@ -1,17 +1,46 @@
 public class HuffmanList {
 
-    // initialize list data structure taking values of type HuffmanNode.
+    private Item min = null;
+
+    private class Item {
+        HuffmanNode node;
+        Item next;
+    }
 
     public void insert(HuffmanNode n) {
-        // inserts new item of value n into the list
+        Item item = new Item();
+        item.node = n;
+        item.next = null;
+        if (min == null) {
+            min = item;
+            return;
+        }
+        item q = null;
+        item p = min;
+        while (p != null && p.node.frequency < n.frequency) {
+            q = p;
+            p = p.next;
+        }
+        if (q == null) {
+            item.next = min;
+            min = item;
+        } else {
+            item.next = p;
+            q.next = item;
+        }
     }
 
     public HuffmanNode extractMin() {
-        // returns the item of the list whose HuffmanNode has minimum value and also deletes this item from the list
+        if (min == null) {
+            return null;
+        }
+        HuffmanNode n = min.node;
+        min = min.next;
+        return n;
     }
 
     public boolean containsAtMostOneItem() {
-        // returns true if the list contains zero or one item, and false otherwise.
+        return (min == null || min.next == null);
     }
 
 }
